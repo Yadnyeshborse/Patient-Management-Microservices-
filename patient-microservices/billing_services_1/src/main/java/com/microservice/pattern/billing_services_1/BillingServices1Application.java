2b33f054533fa48a4d01e582c861d09277065f3b
Yadnyesh Borse <yadvborse27@gmail.com>
2026-01-15 16:15:52 +0530

    I am adding billing service feature

---
+++ patient-microservices/billing_services_1/src/main/java/com/microservice/pattern/billing_services_1/BillingServices1Application.java
@@ -0,0 +1,13 @@
+package com.microservice.pattern.billing_services_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingServices1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillingServices1Application.class, args);
+    }
+
+}
